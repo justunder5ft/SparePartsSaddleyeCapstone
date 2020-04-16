1633f67d9f7116439e4f8a882716a737eecb6198
brice <bricebrosig@gmail.com>
2020-04-16 16:30:54 -0500

    Changes to the data class mode

--- learning/main.py
+++ learning/main.py
@@ -15,6 +15,7 @@ Off-road
 @incomplete - get estimate for training time
 @incomplete - look into cloud computing (if the above is too large)
 """
+from PIL import ImageFile
 from keras.models import Sequential  # this is the model
 from keras.layers import Conv2D, MaxPool2D  # for the convolution layer and pooling layer
 from keras.layers import Flatten, Dense, Dropout, Activation  # some neat layers that help us out 
@@ -23,7 +24,9 @@ from keras.preprocessing.image import ImageDataGenerator  # for loading the imag
 import numpy as np
 import matplotlib.pyplot as plot
 import os  # setting the log level
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 # Image format
 IMG_HEIGHT = 720
@@ -34,14 +37,14 @@ TRAINING_DATA_DIR, TRAINING_DATA_SIZE = 'D:/Training Data/partitioned_data/train
 VALIDATION_DATA_DIR, VALIDATION_DATA_SIZE = 'D:/Training Data/partitioned_data/validate', 9406
 TEST_DATA_DIR, TEST_DATA_SIZE = 'D:/Training Data/partitioned_data/test', 9405
 
-EPOCHS = 5          # Change these as we need to
-BATCH_SIZE = 10     # This doesn't really matter right now since we have like no photos...
+EPOCHS = 5  # Change these as we need to
+BATCH_SIZE = 10  # This doesn't really matter right now since we have like no photos...
 
 if K.image_data_format() == 'channels_first':  # i think channels first is the default so should be fine
     input_shape = (3, IMG_WIDTH, IMG_HEIGHT)
 else:
     input_shape = (IMG_WIDTH, IMG_HEIGHT, 3)
-    
+
 # ===BUILDING THE MODEL===
 model = Sequential()
 model.add(Conv2D(32, (3, 3), input_shape=input_shape))
@@ -60,10 +63,10 @@ model.add(Flatten())
 model.add(Dense(64))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
-model.add(Dense(1))
+model.add(Dense(4))
 model.add(Activation('sigmoid'))
 
-model.compile(loss='binary_crossentropy',
+model.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 
@@ -79,28 +82,28 @@ train_datagen = ImageDataGenerator(
 # only rescaling
 test_datagen = ImageDataGenerator(rescale=1. / 255)  # get the RGB values in between 0 and 1
 
-train_generator = train_datagen.flow_from_directory(        # generate data for the training set
+train_generator = train_datagen.flow_from_directory(  # generate data for the training set
     TRAINING_DATA_DIR,
     target_size=(IMG_WIDTH, IMG_HEIGHT),
     batch_size=BATCH_SIZE,
-    class_mode='binary'
+    class_mode='categorical'
 )
 
-validation_generator = test_datagen.flow_from_directory(    # generate data for the validation set
+validation_generator = test_datagen.flow_from_directory(  # generate data for the validation set
     VALIDATION_DATA_DIR,
     target_size=(IMG_WIDTH, IMG_HEIGHT),
     batch_size=BATCH_SIZE,
-    class_mode='binary'
+    class_mode='categorical'
 )
 
-test_generator = test_datagen.flow_from_directory(          # generate data for the test set
+test_generator = test_datagen.flow_from_directory(  # generate data for the test set
     TEST_DATA_DIR,
     target_size=(IMG_WIDTH, IMG_HEIGHT),
     batch_size=BATCH_SIZE,
-    class_mode='binary'
+    class_mode='categorical'
 )
 
-model.fit_generator(    # training the model
+model.fit_generator(  # training the model
     train_generator,
     steps_per_epoch=TRAINING_DATA_SIZE // BATCH_SIZE,
     epochs=EPOCHS,
